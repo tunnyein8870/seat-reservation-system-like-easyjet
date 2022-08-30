8ce24bb552dc45b118c2cc74769c268ced2fadeb
Tun Nyein <40516600@live.napier.ac.uk>
2022-08-30 21:58:19 +0630

    create html in js and seat select and cancel ok

--- static/seats.js
+++ static/seats.js
@@ -20,5 +20,64 @@ document.body.onload=async()=>{
     <div>Departure: ${basket.JourneyPairs[0].OutboundSlot.Flight.LocalDepartureTime}</div>
     <div>Arrival: ${basket.JourneyPairs[0].OutboundSlot.Flight.LocalArrivalTime.substring(11,16)}</div>
     `;
-    console.log(basket);
+
+    //  create rows, blocks and seat in html
+    let rowcount = 8;
+    let num = ['A', 'B', 'C', 'D', 'E', 'F'];
+    let check = Math.floor (num.length/2);
+    for (let rownum = 1; rownum <= rowcount; rownum++){
+        // create div under center div
+        let divrow = document.createElement('div');
+        divrow.classList = "row"; // create class='row'
+        divrow.setAttribute("row", `row_${rownum}`);  // create row="row_1"
+        let divblock = document.createElement('div');  // create div under div class="row"
+        divblock.classList = "block";
+        for (let i in num){
+            // console.log(num[i], "i");
+            let divseat = document.createElement('div');
+            divseat.classList = "seat";
+            divseat.id = `seat_${rownum}${num[i]}`;
+            // console.log(`seat_${rownum}${num[i]}`);
+            // insert row number between seats
+            if (i == check){
+                let rowshow = document.createElement('div');
+                rowshow.classList = "rowshow";
+                rowshow.innerHTML = `${rownum}`
+                // divblock.append(`${rownum}`);
+                divblock.append(rowshow);
+            }
+            divblock.append(divseat);
+            console.log(i);
+        }
+        console.log(check);
+        divrow.append(divblock);
+        document.getElementById('center').append(divrow);
+    }
+    //Mark seats as available or not-available
+    for(let r=0;r<seats.Rows.length;r++){
+        for(let b=0;b<seats.Rows[r].Blocks.length;b++){
+            for(let s=0; s < seats.Rows[r].Blocks[b].Seats.length;s++){
+                // console.log(seats.Rows[r].Blocks[b].Seats[s]);
+                let seat_id = `seat_${seats.Rows[r].Blocks[b].Seats[s].SeatNumber}`;
+                let seat_div = document.getElementById(seat_id);
+                if (seat_div){
+                    if (seats.Rows[r].Blocks[b].Seats[s].IsAvailable){
+                        seat_div.classList.add('available');
+                    }else{
+                        seat_div.classList.add('unavailable');
+                    }
+                    seat_div.onclick = ()=>{
+                        // select seat or cancel
+                        if (seat_div.classList.contains('occupied')){
+                            seat_div.classList.remove('occupied');
+                        }
+                        else{
+                            seat_div.classList.add('occupied');
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
